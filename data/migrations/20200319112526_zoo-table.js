0cd6d70680d381c5ba99571eb8213df7d9a4813e
LISA_COOK <lisabpink@gmail.com>
2020-03-19 12:18:34 -0400

    LDB npm i helmet, update exports

--- data/migrations/20200319112526_zoo-table.js
+++ data/migrations/20200319112526_zoo-table.js
@@ -1,8 +1,59 @@
-
 exports.up = function(knex) {
+    return knex.schema.createTable('species', tbl => {
+         // the type of Primary Key is: integer without negative values, also called unsigned
+        tbl.increments();
   
-};
-
-exports.down = function(knex) {
+        tbl.string('name', 255).notNullable();
+    })
+    .createTable('animals', tbl => {
+        tbl.increments();
   
-};
+        tbl.string('name', 255).notNullable();
+  
+        // define our Foreign Keys
+        tbl
+          .integer('species_id')
+          .unsigned()
+          .references('id')
+          .inTable('species')
+          .onDelete('RESTRICT') // about deleting the record of the primary key table. 'CASCADE', 'RESTRICT', 'NO ACTION', 'SET NULL'
+          .onUpdate('CASCADE') // about changing the value of the primary key table
+      
+          // we have bears and a few animals that are bears
+    })
+    .createTable('zoos', tbl => {
+      tbl.increments();
+      tbl.string('name', 255).notNullable();
+      tbl.string('address', 255).notNullable();
+    })
+    .createTable('animal_zoos', tbl => {
+        tbl.increments()
+        
+        tbl
+          .integer('zoo_id')
+          .unsigned()
+          .references('id')
+          .inTable('zoos')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+  
+        tbl
+          .integer('animal_id')
+          .unsigned()
+          .references('id')
+          .inTable('animals')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+  
+          tbl.date('from');
+          tbl.date('to');
+    })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('animal_zoos')
+      .dropTableIfExists('zoos')
+      .dropTableIfExists('animals')
+      .dropTableIfExists('species');
+  };
